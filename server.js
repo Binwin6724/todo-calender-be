f3e769d4c37a3e36643cd7876939bfd26185003f
Binwin6724 <binwinviju225096@gmail.com>
2025-08-28 03:11:39 +0530

    feat: configure CORS with allowed origins, credentials, methods and headers

--- server.js
+++ server.js
@@ -23,7 +23,12 @@ let db;
 let tasksCollection;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:3000', 'http://localhost:3001', 'http://127.0.0.1:3000', 'http://127.0.0.1:3001', 'https://todo-calender.up.railway.app/'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
+}));
 app.use(express.json());
 
 // Connect to MongoDB
